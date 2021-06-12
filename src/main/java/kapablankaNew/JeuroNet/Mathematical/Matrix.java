2f7944262ceb1b23c9cc6c608a1aa4080784af7e
Konstantin Konovalov <kodmko@mail.ru>
2021-06-13 00:07:24 +0300

    Version 1.3.5. Created class Matrix. Now this class contains constructors and methods for getting sizes and elements of the matrix.

---
+++ src/main/java/kapablankaNew/JeuroNet/Mathematical/Matrix.java
@@ -0,0 +1,55 @@
+package kapablankaNew.JeuroNet.Mathematical;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matrix {
+    @Getter
+    private final int rows;
+
+    @Getter
+    private final int columns;
+
+    private final List<List<Double>> elements;
+
+    public Matrix(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        elements = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            List<Double> row = new ArrayList<>();
+            for (int j = 0; j < columns; j++) {
+                row.add(0.0);
+            }
+            elements.add(row);
+        }
+    }
+
+    public Matrix (int rows, int columns, List<List<Double>> elements) throws VectorMatrixException {
+        if (elements.size() != rows) {
+            throw new VectorMatrixException("The size of the data is not equal to the size of the matrix.");
+        }
+        for (int i = 0; i < rows; i++) {
+            if (elements.get(i).size() != columns) {
+                throw new VectorMatrixException("The size of the data is not equal to the size of the matrix.");
+            }
+        }
+        this.rows = rows;
+        this.columns = columns;
+
+        this.elements = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            List<Double> row = new ArrayList<>();
+            for (int j = 0; j < columns; j++) {
+                row.add(elements.get(i).get(j));
+            }
+            this.elements.add(row);
+        }
+    }
+
+    public double get(int row, int column) {
+        return elements.get(row).get(column);
+    }
+}
